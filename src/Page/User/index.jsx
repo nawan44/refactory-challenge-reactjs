becc208c4b3910bdfd51996588fb4a4ced09930c
Rachmat Gunawan <rachmat.d.gunawan@gmail.com>
2021-03-28 19:32:03 +0700

    get data user

--- src/Page/User/index.jsx
+++ src/Page/User/index.jsx
@@ -6,63 +6,78 @@ import {
   TableCell,
   TableRow, TableBody
 } from "@material-ui/core";
-import React from "react";
+import Axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const useStyles = makeStyles({
   root: {
     width: "100%",
     margin: "100px 0 0 0 ",
-    backgroundColor: "red",
+    // backgroundColor: "red",
+    textAlign:"center",
+    margin:"0 auto"
   },
+  paper:{
+    textAlign:"center",
+    margin:"0 auto",
+    width:"800px",
+  },
+  title: {
+    fontSize:"20px",
+    padding:"20px 0"
+  },
+  head:{
+    fontSize:"16px",
+  }
 });
 
 export default function User() {
   const classes = useStyles();
+  const [user, setUser] = useState([]);
+// console.log(user)
+  useEffect(() => {
+    Axios.get("https://jsonplaceholder.typicode.com/users").then(res => {
+      setUser(res.data);
+    });
+  });
   return (
     <div className={classes.root}>
-      <Paper>
+      <Paper className={classes.paper}>
+        <h3 className={classes.title}>DATA USER</h3>
         <Table>
           <TableHead>
             <TableRow>
-              <TableCell>Name</TableCell>
-              <TableCell>Username</TableCell>
-              <TableCell>Email</TableCell>
-              <TableCell>Address</TableCell>
-              <TableCell>Phone</TableCell>
-              <TableCell>Website</TableCell>
-              <TableCell>Company</TableCell>
-            </TableRow>{" "}
+            <TableCell  className={classes.head}>ID</TableCell>
+              
+              <TableCell  className={classes.head}>Name</TableCell>
+              <TableCell  className={classes.head}>Username</TableCell>
+              <TableCell  className={classes.head}>Email</TableCell>
+              <TableCell  className={classes.head}>Address</TableCell>
+              <TableCell  className={classes.head}>Phone</TableCell>
+              {/* <TableCell  className={classes.head}>Website</TableCell> */}
+              {/* <TableCell  className={classes.head}>Company</TableCell> */}
+            </TableRow>
           </TableHead>
           <TableBody>
+          {user.map(item => (
             <TableRow>
-              <TableCell>Name</TableCell>
-              <TableCell>Username</TableCell>
-              <TableCell>Email</TableCell>
-              <TableCell>Address</TableCell>
-              <TableCell>Phone</TableCell>
-              <TableCell>Website</TableCell>
-              <TableCell>Company</TableCell>
-            </TableRow>{" "}
-            <TableRow>
-              <TableCell>Name</TableCell>
-              <TableCell>Username</TableCell>
-              <TableCell>Email</TableCell>
-              <TableCell>Address</TableCell>
-              <TableCell>Phone</TableCell>
-              <TableCell>Website</TableCell>
-              <TableCell>Company</TableCell>
+              <TableCell>{item.id}</TableCell>
+              
+              <TableCell>{item.name}</TableCell>
+              <TableCell>{item.username}</TableCell>
+              <TableCell>{item.email}</TableCell>
+              <TableCell>{item.address.street}{" "+item.address.suite+" "}{item.address.city}</TableCell>
+              <TableCell>{item.phone}</TableCell>
+              {/* <TableCell>{item.website}</TableCell> */}
+              {/* <TableCell>{item.company.name}{" "+item.company.catchPhrase+" "}{item.company.bs}</TableCell> */}
             </TableRow>
-            <TableRow>
-              <TableCell>Name</TableCell>
-              <TableCell>Username</TableCell>
-              <TableCell>Email</TableCell>
-              <TableCell>Address</TableCell>
-              <TableCell>Phone</TableCell>
-              <TableCell>Website</TableCell>
-              <TableCell>Company</TableCell>
-            </TableRow>
+              ))}
           </TableBody>
- 
+          <ul>
+        
+         
+     
+        </ul>
         </Table>
       </Paper>
     </div>
