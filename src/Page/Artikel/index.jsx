66e0ae59c7d2d85fb484232f534c23711d8c72d5
Rachmat Gunawan <rachmat.d.gunawan@gmail.com>
2021-04-15 23:41:49 +0700

    external css : article

--- src/Page/Artikel/index.jsx
+++ src/Page/Artikel/index.jsx
@@ -1,50 +1,11 @@
 import Axios from "axios";
 import React, { useEffect, useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Button from "@material-ui/core/Button";
+import {Card, CardActionArea, CardActions,  CardContent,CardMedia, Button } from "@material-ui/core";
 import Image1 from "../../Assets/Image/img1.jpg";
 
-const useStyles = makeStyles({
-  root: {
-    width: 700,
-    textAlign: "center",
-    margin: "0 auto",
-    backgroundColor: "#fff",
-  },
-  cardArea: {
-    // backgroundColor:"red",
-    height: "300px",
-  },
-  title: {
-    width: "100%",
-    fontSize: "30px",
-    fontWeight: 500,
-    lineHeight: "16px",
-    padding: "5px 0 ",
-  },
-  cardContent: {
-    width: "100%",
-  },
-  cardMedia: {
-    float: "left",
-    width: "30%",
-  },
-  cardDesc: {
-    float: "left",
-    width: "70%",
-    fontSize: "16px",
-    padding: "0 10px",
-    textAlign: "left",
-  },
-});
+
 
 export default function ImgMediaCard() {
-  const classes = useStyles();
   const [article, setArticle] = useState([]);
   console.log(article);
   useEffect(() => {
@@ -55,23 +16,23 @@ export default function ImgMediaCard() {
   return (
     <div>
       {article.map((item) => (
-        <Card className={classes.root}>
-          <CardActionArea className={classes.cardArea}>
-            <h1 className={classes.title}>{item.name}</h1>
-            <CardContent className={classes.cardContent}>
+        <Card className="rootArticle">
+          <CardActionArea className="cardArea">
+            <h1 className="title">{item.name}</h1>
+            <CardContent className="cardContent">
               <CardMedia
                 component="img"
                 alt="Contemplative Reptile"
                 height="140"
                 image={Image1}
                 title="Contemplative Reptile"
-                className={classes.cardMedia}
+                className="cardMedia"
               />
               <span
                 variant="body2"
                 color="textSecondary"
                 component="p"
-                className={classes.cardDesc}
+                className="cardDesc"
               >
                 {item.address.street}
                 {" " + item.address.suite + " "}
