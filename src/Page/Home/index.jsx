1d31213e933a3d146bad1359bf54751839befaed
Rachmat Gunawan <rachmat.d.gunawan@gmail.com>
2021-04-18 23:59:43 +0700

    switch css

--- src/Page/Home/index.jsx
+++ src/Page/Home/index.jsx
@@ -3,12 +3,14 @@ import {
   Table,
   Paper,
   TableCell,
-  TableRow, TableBody, FormControlLabel, Switch, Button
+  TableRow,
+  TableBody,
+  FormControlLabel,
+  Switch,
+  Button,
 } from "@material-ui/core";
-import Axios from 'axios';
-import React, { useEffect, useState } from 'react';
-
-
+import Axios from "axios";
+import React, { useEffect, useState } from "react";
 
 export default function Home() {
   const [todo, setTodo] = useState([]);
@@ -18,9 +20,9 @@ export default function Home() {
   const handleChange = (event) => {
     setState({ ...state, [event.target.name]: event.target.checked });
   };
-// console.log(user)
+  // console.log(user)
   useEffect(() => {
-    Axios.get("https://jsonplaceholder.typicode.com/todos").then(res => {
+    Axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
       setTodo(res.data);
     });
   });
@@ -28,40 +30,40 @@ export default function Home() {
     <div className="root">
       <Paper className="paper">
         <h3 className="title">TODO LIST</h3>
-        <Button  color="primary"className="button" href="input-todo-list">add TODO LIST</Button>
+        <Button color="primary" className="button" href="input-todo-list">
+          add TODO LIST
+        </Button>
 
         <Table>
           <TableHead>
             <TableRow>
-            <TableCell  className="head">User Id</TableCell>           
-              <TableCell  className="head">Id</TableCell>
-              <TableCell  className="head">Title</TableCell>
-              <TableCell  className="head">Completed</TableCell>   
+              <TableCell className="head">User Id</TableCell>
+              <TableCell className="head">Id</TableCell>
+              <TableCell className="head">Title</TableCell>
+              <TableCell className="headCompleted">Status</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-          {todo.map(item => (
-            <TableRow>
-              <TableCell className="list" >{item.userId}</TableCell>
-              <TableCell className="list">{item.id}</TableCell>
-              <TableCell className="list">{item.title}</TableCell>
-              <TableCell >
-              <FormControlLabel
-              
-        control={
-          <Switch
-            checked={item.completed}
-            onChange={handleChange}
-            name="checkedB"
-            value={state.checkedB}
-            color="primary"
-          />
-        }
-        label="Completed"
-      />
-              </TableCell>
-            </TableRow>
-              ))}
+            {todo.map((item) => (
+              <TableRow>
+                <TableCell className="list">{item.userId}</TableCell>
+                <TableCell className="list">{item.id}</TableCell>
+                <TableCell className="list">{item.title}</TableCell>
+                <TableCell  className="headCompleted">
+                  <FormControlLabel
+                    control={
+                      <Switch
+                      className="switch"
+                        checked={state.checkedB}
+                        onChange={handleChange}
+                        name="checkedB"
+                      />
+                    }
+                    label="Finish"
+                  />
+                </TableCell>
+              </TableRow>
+            ))}
           </TableBody>
         </Table>
       </Paper>
